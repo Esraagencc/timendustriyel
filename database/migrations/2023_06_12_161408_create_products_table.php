d2e51e49cc16c62c0c5d6558f2367dde2e61c111
Esraagencc <100173914+Esraagencc@users.noreply.github.com>
2023-06-12 19:46:40 +0300

    product table eklendi

---
+++ database/migrations/2023_06_12_161408_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('size');
+            $table->string('materials');
+            $table->string('pressure_class');
+            $table->string('connections');
+            $table->string('characteristic');
+            $table->string('process_temperature');
+            $table->string('design_standard');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
